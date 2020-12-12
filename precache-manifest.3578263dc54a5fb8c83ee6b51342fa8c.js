f695cc3d29dadd4ffe50f72ee37b53bc689af125
Tachrade <tachradeprogrammer@gmail.com>
2020-12-12 12:48:56 +0200

    Updates

---
+++ precache-manifest.3578263dc54a5fb8c83ee6b51342fa8c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8e4a4669451dd036bc049a560bd7697",
+    "url": "/CreatePostView/index.html"
+  },
+  {
+    "revision": "67a6b61e3c815ed25f6c",
+    "url": "/CreatePostView/static/js/2.180d86fb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/CreatePostView/static/js/2.180d86fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74e38e2b24dca1daaf6d",
+    "url": "/CreatePostView/static/js/main.8c946f1b.chunk.js"
+  },
+  {
+    "revision": "6399cf9075de454997ea",
+    "url": "/CreatePostView/static/js/runtime-main.34bcf6c5.js"
+  }
+]);
